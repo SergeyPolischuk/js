39c3157959f317aa50d167bb11b148924f518708
Serg <polischuksg@gmail.com>
2022-07-27 20:05:35 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// const obj = {
+//     Anna: 500,
+//     Alice: 800
+// }
+
+// obj.Bond = 1000;
+// console.log(obj.Bond)
+
+
+const category = 'toys';
+
+let url = `https://someurl.com/${category}/5`
+
+console.log(url)
