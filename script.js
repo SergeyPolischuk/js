9d96416462c4be6dbf8b7027a5bcdae243abb168
Serhii <polischuksg@gmail.com>
2022-07-28 20:56:55 +0300

    lesson19

--- script.js
+++ script.js
@@ -8,9 +8,28 @@
 // obj.Bond = 1000;
 // console.log(obj.Bond)
 
+// const category = 'toys';
 
-const category = 'toys';
+// let url = `https://someurl.com/${category}/5`
 
-let url = `https://someurl.com/${category}/5`
+// console.log(url)
 
-console.log(url)
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", 0);
+
+const persolnalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let movie_name = prompt("Один из последних просмотренных фильмов?", ''),
+    movie_rate = +prompt("На сколько оцените его?", 0),
+    movie_name2 = prompt("Один из последних просмотренных фильмов?", ''),
+    movie_rate2 = +prompt("На сколько оцените его?", 0);
+
+persolnalMovieDB.movies[movie_name] = movie_rate
+persolnalMovieDB.movies[movie_name2] = movie_rate2
+
+console.log(persolnalMovieDB)
